b98857be513d5692f7fe6a8b2c9a359c5bf71765
Jesse Struck <jdstruck@gmail.com>
2019-11-09 14:44:07 -0500

    create basic 'landmarks' template

---
+++ EFM/EFM/LandmarkDetail.swift
@@ -0,0 +1,47 @@
+//
+//  LandmarkDetail.swift
+//  EFM
+//
+//  Created by Jesse Struck on 11/8/19.
+//  Copyright © 2019 Jesse Struck. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+
+    var body: some View {
+        VStack {
+            MapView(coordinate: landmark.locationCoordinate)
+                .frame(height: 300)
+
+            CircleImage(image: landmark.image)
+                .offset(x: 0, y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+
+                HStack(alignment: .top) {
+                    Text(landmark.park)
+                        .font(.subheadline)
+                    Spacer()
+                    Text(landmark.state)
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+
+            Spacer()
+        }
+        .navigationBarTitle(Text(verbatim: landmark.name), displayMode: .inline)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: landmarkData[0])
+    }
+}
